b809a1624014d1e053d7bce0aa62fd64796f5e58
jabeler <31429009+jabeler@users.noreply.github.com>
2019-09-03 15:32:23 -0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require 'fastlane_core/helper'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
